221c4e4b6384b777de91f23b6dc72ffdedea1cd4
sergeipavlov <s.n.pavlov@outlook.com>
2024-09-25 23:40:51 +0300

    Start project 2024-09-25

---
+++ app/src/main/java/ru/sergeipavlov/metrology/MainActivity.java
@@ -0,0 +1,58 @@
+package ru.sergeipavlov.metrology;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import ru.sergeipavlov.metrology.kip.ScaleSignalActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ScaleSignalActivity.class);
+                v.getContext().startActivity(intent);
+            }
+        });
+
+    }
+
+
+
+//        button.setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            Intent intent = new Intent(v.getContext(), ScaleSignalActivity.class);
+//            v.getContext().startActivity(intent);
+////                Intent intent = new Intent(view.getContext(), AnotherActivity.class);
+////                view.getContext().startActivity(intent);
+//        }
+//    });
+}
